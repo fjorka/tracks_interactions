3bb9bf3d13f8b6673cd095cb4fa504d625033d47
Kasia <7549583+fjorka@users.noreply.github.com>
2024-01-10 15:02:40 -0500

    project setup changes

--- libraries/cells_database.py
+++ libraries/cells_database.py
@@ -27,8 +27,8 @@ NO_SHAPE = -1
 
 Base = declarative_base()
 
-class CellDB(Base):
 
+class CellDB(Base):
     __tablename__ = "cells"
 
     track_id = Column(Integer, ForeignKey('tracks.track_id'), primary_key=True)
@@ -40,32 +40,30 @@ class CellDB(Base):
     row = Column(Integer)
     col = Column(Integer)
 
-    bbox_0 = Column(Integer, default = NO_SHAPE)
-    bbox_1 = Column(Integer, default = NO_SHAPE)
-    bbox_2 = Column(Integer, default = NO_SHAPE)
-    bbox_3 = Column(Integer, default = NO_SHAPE)
+    bbox_0 = Column(Integer, default=NO_SHAPE)
+    bbox_1 = Column(Integer, default=NO_SHAPE)
+    bbox_2 = Column(Integer, default=NO_SHAPE)
+    bbox_3 = Column(Integer, default=NO_SHAPE)
 
-    mask = Column(PickleType,default = NO_SHAPE)
-
+    mask = Column(PickleType, default=NO_SHAPE)
 
     def __repr__(self):
         return f"{self.id} from frame {self.t} with track_id {self.track_id} at ({self.row},{self.col})"
 
-class TrackDB(Base):
 
+class TrackDB(Base):
     __tablename__ = 'tracks'
 
     track_id = Column(Integer, primary_key=True)
     parent_track_id = Column(Integer)
 
     # indexed to speed up queries for entire families
-    root = Column(Integer, index = True)
+    root = Column(Integer, index=True)
 
     t_begin = Column(Integer)
-    t_end = Column(Integer) 
+    t_end = Column(Integer)
 
     def __repr__(self):
-
         return f"Track {self.track_id} from {self.t_begin} to {self.t_end}"
 
 
@@ -112,7 +110,6 @@ def _fast_forest_transverse(
 
 
 def add_track_ids_to_tracks_df(df: pd.DataFrame) -> pd.DataFrame:
-
     """Adds `track_id` and `parent_track_id` columns to forest `df`.
     Each maximal path receveis a unique `track_id`.
 
@@ -137,12 +134,20 @@ def add_track_ids_to_tracks_df(df: pd.DataFrame) -> pd.DataFrame:
     df["track_id"] = NO_PARENT
     df["parent_track_id"] = NO_PARENT
 
-    paths, track_ids, parent_track_ids, lengths, roots_list = _fast_forest_transverse(roots, forest)
+    (
+        paths,
+        track_ids,
+        parent_track_ids,
+        lengths,
+        roots_list,
+    ) = _fast_forest_transverse(roots, forest)
 
     paths = np.concatenate(paths)
     df.loc[paths, "track_id"] = np.repeat(track_ids, lengths)
     df.loc[paths, "parent_track_id"] = np.repeat(parent_track_ids, lengths)
-    df.loc[paths, "root"] = df.loc[np.repeat(roots_list, lengths),'track_id'].tolist()
+    df.loc[paths, "root"] = df.loc[
+        np.repeat(roots_list, lengths), 'track_id'
+    ].tolist()
 
     unlabeled_tracks = df["track_id"] == NO_PARENT
     assert not np.any(
@@ -150,4 +155,3 @@ def add_track_ids_to_tracks_df(df: pd.DataFrame) -> pd.DataFrame:
     ), f"Something went wrong. Found unlabeled tracks\n{df[unlabeled_tracks]}"
 
     return df
-
