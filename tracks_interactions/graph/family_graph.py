ddc5b7371c6d09759993985a939c36fb988be310
Kasia <7549583+fjorka@users.noreply.github.com>
2024-02-08 13:53:32 -0500

    graph in class

--- tracks_interactions/graph/family_graph.py
+++ tracks_interactions/graph/family_graph.py
@@ -1,10 +1,147 @@
 import numpy as np
 import pandas as pd
-import pyqtgraph as pg
 from ete3 import Tree
 from PyQt5.QtCore import Qt
+from pyqtgraph import GraphicsLayoutWidget, TextItem, mkColor, mkPen
 
 from tracks_interactions.db.db_model import TrackDB
+
+
+class FamilyGraph(GraphicsLayoutWidget):
+    def __init__(self, viewer, session):
+        super().__init__()
+
+        self.session = session
+        self.viewer = viewer
+        self.labels_layer = self.viewer.layers["Labels"]
+
+        # initialize graph
+        self.plot_view = self.addPlot(
+            title="Lineage tree", labels={"bottom": "Time"}
+        )
+        self.plot_view.hideAxis("left")
+
+        self.t_max = self.viewer.dims.range[0][1]
+        self.plot_view.setXRange(0, self.t_max)
+
+        # initialize time line
+        pen = mkPen(color=(255, 255, 255), xwidth=1)
+        init_position = self.viewer.dims.current_step[0]
+        self.time_line = self.plot_view.addLine(x=init_position, pen=pen)
+
+        # connect time slider event
+        self.viewer.dims.events.current_step.connect(self.update_family_line)
+
+        # connect label selection event
+        self.labels_layer.events.selected_label.connect(
+            self.update_lineage_display
+        )
+
+    def update_family_line(self):
+        """
+        Update of the family line when slider position is moved.
+        """
+        # line_position = event.value # that emitts warning for reasons that I don't understand
+        line_position = self.viewer.dims.current_step[0]
+        self.time_line.setValue(line_position)
+
+    def update_lineage_display(self):
+        """
+        Update of the lineage display when a new label is selected.
+        """
+
+        # Clear all elements except the time line
+        items_to_remove = self.plot_view.items[
+            1:
+        ]  # Get all items except the time line
+        for item in items_to_remove:
+            self.plot_view.removeItem(item)
+
+        # get an active label
+        active_label = int(self.viewer.layers["Labels"].selected_label)
+
+        # check if the label is in the database
+        query = (
+            self.session.query(TrackDB)
+            .filter(TrackDB.track_id == active_label)
+            .first()
+        )
+
+        # actions based on finding the label in the database
+        if query is not None:
+            # get the root value
+            root = query.root
+
+            # update viewer status
+            self.viewer.status = f"Family of track number {root}."
+
+            # buid the tree
+            tree = build_Newick_tree(self.session, root)
+
+            # update the widget with the tree
+            self.render_tree_view(tree)
+
+        else:
+            self.viewer.status = "Error - no such label in the database."
+
+    def render_tree_view(self, t):
+        """
+        Create a new family tree when the update is called.
+        """
+
+        y_max = 1
+        active_label = int(self.viewer.layers["Labels"].selected_label)
+
+        # render the tree
+        t_rendering = t.render(".")
+
+        # add position of y to the rendering
+        t = _add_y_rendering(t, t_rendering)
+
+        for n in t.traverse():
+            if n.is_root():
+                pass
+            else:
+                node_name = n.name
+
+                # get position in time
+                x1 = n.start
+                x2 = n.stop
+                x_signal = [x1, x2]
+
+                # get rendered position (y axis)
+                y_signal = n.y.repeat(2)
+                y_max = np.max([n.y, y_max])
+
+                label_color = self.labels_layer.get_color(node_name)
+                if node_name == active_label:
+                    pen = mkPen(
+                        color=mkColor((label_color * 255).astype(int)), width=5
+                    )
+
+                else:
+                    label_color[-1] = 0.6
+                    pen = mkPen(
+                        color=mkColor((label_color * 255).astype(int)), width=4
+                    )
+                    pen.setStyle(Qt.DotLine)
+
+                self.plot_view.plot(x_signal, y_signal, pen=pen)
+
+                text_item = TextItem(str(node_name), anchor=(1, 1))
+                text_item.setPos(x2, n.y)
+                self.plot_view.addItem(text_item)
+
+                # check if children are present
+                if len(n.children) > 0:
+                    for child in n.children:
+                        x_signal = [x2, x2]
+                        y_signal = [n.y, child.y]
+                        self.plot_view.plot(x_signal, y_signal, pen=pen)
+
+        # set limits on axis
+        self.plot_view.setXRange(0, self.t_max)
+        self.plot_view.setYRange(0, 1.1 * y_max)
 
 
 def _add_children(node, df, n=2):
@@ -95,138 +232,125 @@ def build_Newick_tree(session, root_id):
     return tree
 
 
-def render_tree_view(plot_view, t, viewer):
-    """
-    Input:
-    plot_view - from plot_widget
-    t - tree object
-    viewer - general napari viewer (to get label colors)
-    Output:
-    plot_view - a plot with the tree rendered on it.
-    """
+# def render_tree_view(plot_view, t, viewer):
+#     """
+#     Input:
+#     plot_view - from plot_widget
+#     t - tree object
+#     viewer - general napari viewer (to get label colors)
+#     Output:
+#     plot_view - a plot with the tree rendered on it.
+#     """
 
-    labels_layer = viewer.layers["Labels"]
-    active_label = int(viewer.layers["Labels"].selected_label)
+#     labels_layer = viewer.layers["Labels"]
+#     active_label = int(viewer.layers["Labels"].selected_label)
 
-    y_max = 1
+#     y_max = 1
 
-    # render the tree
-    t_rendering = t.render(".")
+#     # render the tree
+#     t_rendering = t.render(".")
 
-    # add position of y to the rendering
-    t = _add_y_rendering(t, t_rendering)
+#     # add position of y to the rendering
+#     t = _add_y_rendering(t, t_rendering)
 
-    for n in t.traverse():
-        if n.is_root():
-            pass
-        else:
-            node_name = n.name
+#     for n in t.traverse():
+#         if n.is_root():
+#             pass
+#         else:
+#             node_name = n.name
 
-            # get position in time
-            x1 = n.start
-            x2 = n.stop
-            x_signal = [x1, x2]
+#             # get position in time
+#             x1 = n.start
+#             x2 = n.stop
+#             x_signal = [x1, x2]
 
-            # get rendered position (y axis)
-            y_signal = n.y.repeat(2)
-            y_max = np.max([n.y, y_max])
+#             # get rendered position (y axis)
+#             y_signal = n.y.repeat(2)
+#             y_max = np.max([n.y, y_max])
 
-            label_color = labels_layer.get_color(node_name)
-            if node_name == active_label:
-                pen = pg.mkPen(
-                    color=pg.mkColor((label_color * 255).astype(int)), width=5
-                )
+#             label_color = labels_layer.get_color(node_name)
+#             if node_name == active_label:
+#                 pen = pg.mkPen(
+#                     color=pg.mkColor((label_color * 255).astype(int)), width=5
+#                 )
 
-            else:
-                label_color[-1] = 0.6
-                pen = pg.mkPen(
-                    color=pg.mkColor((label_color * 255).astype(int)), width=4
-                )
-                pen.setStyle(Qt.DotLine)
+#             else:
+#                 label_color[-1] = 0.6
+#                 pen = pg.mkPen(
+#                     color=pg.mkColor((label_color * 255).astype(int)), width=4
+#                 )
+#                 pen.setStyle(Qt.DotLine)
 
-            plot_view.plot(x_signal, y_signal, pen=pen)
+#             plot_view.plot(x_signal, y_signal, pen=pen)
 
-            text_item = pg.TextItem(str(node_name), anchor=(1, 1))
-            text_item.setPos(x2, n.y)
-            plot_view.addItem(text_item)
+#             text_item = pg.TextItem(str(node_name), anchor=(1, 1))
+#             text_item.setPos(x2, n.y)
+#             plot_view.addItem(text_item)
 
-            # check if children are present
-            if len(n.children) > 0:
-                for child in n.children:
-                    x_signal = [x2, x2]
-                    y_signal = [n.y, child.y]
-                    plot_view.plot(x_signal, y_signal, pen=pen)
+#             # check if children are present
+#             if len(n.children) > 0:
+#                 for child in n.children:
+#                     x_signal = [x2, x2]
+#                     y_signal = [n.y, child.y]
+#                     plot_view.plot(x_signal, y_signal, pen=pen)
 
-    # set limits on axis
-    t_max = viewer.dims.range[0][1]
-    plot_view.setXRange(0, t_max)
-    plot_view.setYRange(0, 1.1 * y_max)
+#     # set limits on axis
+#     t_max = viewer.dims.range[0][1]
+#     plot_view.setXRange(0, t_max)
+#     plot_view.setYRange(0, 1.1 * y_max)
 
-    return plot_view
-
-
-def build_lineage_widget(t_max):
-    """
-    Builds pyqt widget to display lineage tree.
-    """
-
-    plot_widget = pg.GraphicsLayoutWidget()
-    plot_view = plot_widget.addPlot(
-        title="Lineage tree", labels={"bottom": "Time"}
-    )
-    plot_view.hideAxis("left")
-    plot_view.setXRange(0, t_max)
-
-    return plot_widget
+#     return plot_view
 
 
-def update_lineage_display(viewer, plot_widget, session):
-    """
-    This function is called by the even handler
-    when the user selects a label in the napari viewer.
-    """
+# def build_lineage_widget(t_max):
+#     """
+#     Builds pyqt widget to display lineage tree.
+#     """
 
-    # clear the widget
-    plot_view = plot_widget.getItem(0, 0)
-    # Clear all elements except the last one
-    items_to_remove = plot_view.items[1:]  # Get all items except the time line
-    for item in items_to_remove:
-        plot_view.removeItem(item)
+#     plot_widget = pg.GraphicsLayoutWidget()
+#     plot_view = plot_widget.addPlot(
+#         title="Lineage tree", labels={"bottom": "Time"}
+#     )
+#     plot_view.hideAxis("left")
+#     plot_view.setXRange(0, t_max)
 
-    # get an active label
-    active_label = int(viewer.layers["Labels"].selected_label)
-
-    # check if the label is in the database
-    query = (
-        session.query(TrackDB).filter(TrackDB.track_id == active_label).first()
-    )
-
-    # actions based on finding the label in the database
-    if query is not None:
-        # get the root value
-        root = query.root
-
-        # update viewer status
-        viewer.status = f"Family of track number {root}."
-
-        # buid the tree
-        tree = build_Newick_tree(session, root)
-
-        # update the widget with the tree
-        plot_view = render_tree_view(plot_view, tree, viewer)
-
-    else:
-        viewer.status = "Error - no such label in the database."
+#     return plot_widget
 
 
-def update_family_line(plot_widget, slider_position):
-    """
-    This function is called by the event handler
-    when there is a change of the time point.
-    """
+# def update_lineage_display(viewer, plot_widget, session):
+#     """
+#     This function is called by the even handler
+#     when the user selects a label in the napari viewer.
+#     """
 
-    # get direct access to the line
-    plot_view = plot_widget.getItem(0, 0)
-    time_line = plot_view.items[0]
+#     # clear the widget
+#     plot_view = plot_widget.getItem(0, 0)
+#     # Clear all elements except the last one
+#     items_to_remove = plot_view.items[1:]  # Get all items except the time line
+#     for item in items_to_remove:
+#         plot_view.removeItem(item)
 
-    time_line.setValue(slider_position)
+#     # get an active label
+#     active_label = int(viewer.layers["Labels"].selected_label)
+
+#     # check if the label is in the database
+#     query = (
+#         session.query(TrackDB).filter(TrackDB.track_id == active_label).first()
+#     )
+
+#     # actions based on finding the label in the database
+#     if query is not None:
+#         # get the root value
+#         root = query.root
+
+#         # update viewer status
+#         viewer.status = f"Family of track number {root}."
+
+#         # buid the tree
+#         tree = build_Newick_tree(session, root)
+
+#         # update the widget with the tree
+#         plot_view = render_tree_view(plot_view, tree, viewer)
+
+#     else:
+#         viewer.status = "Error - no such label in the database."
