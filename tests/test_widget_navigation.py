7705be42028b32a36a91ff5f8678666b66224fef
Kasia <7549583+fjorka@users.noreply.github.com>
2024-05-13 10:46:06 -0400

    tests update

--- tests/test_widget_navigation.py
+++ tests/test_widget_navigation.py
@@ -1,18 +1,13 @@
-import numpy as np
-
-from sqlalchemy import MetaData, create_engine, inspect
-from sqlalchemy.orm import make_transient, sessionmaker
-
-import pytest
 from unittest.mock import Mock
 
-from vispy.util.event import Event
+import numpy as np
+import pytest
+from sqlalchemy import MetaData, create_engine, inspect
+from sqlalchemy.orm import sessionmaker
 
-from tracks_interactions.db.db_model import NO_PARENT, CellDB, TrackDB
+from tracks_interactions.db.db_model import CellDB, TrackDB
 from tracks_interactions.widget.widget_navigation import TrackNavigationWidget
 
-import napari
-from qtpy.QtCore import Qt
 
 @pytest.fixture(scope='function')
 def db_session():
@@ -59,48 +54,51 @@ def db_session():
 
     original_session.close()
 
-    yield memory_session  
+    yield memory_session
 
     memory_session.close()
+
 
 @pytest.fixture(scope='function')
 def viewer(make_napari_viewer):
 
     viewer = make_napari_viewer()
-    viewer.add_labels(data=np.zeros([10000,10000],dtype=int))
+    viewer.add_labels(data=np.zeros([10000, 10000], dtype=int))
 
     yield viewer
 
     viewer.close()
 
-def test_select_label(qtbot,viewer,db_session):
+
+def test_select_label(qtbot, viewer, db_session):
     """
     Test selection of a cell with a right click.
-    """    
+    """
 
-    track_navigation_widget = TrackNavigationWidget(viewer,db_session)
+    track_navigation_widget = TrackNavigationWidget(viewer, db_session)
 
     # position for a cell to test selection
     viewer.dims.set_point(0, 10)
-       canvas: SceneCanvas = viewer.window.qt_viewer.canvas
-    # Translate position to canvas coordinates
-    vispy_pos = vispy_utils.qt_to_vispy(pos, canvas)
-    # Create the mouse event with the right button
-    mouse_event = Event(type='mouse_press', pos=vispy_pos, button=2, modifiers=[])
-    # Emit the mouse press event through the visual canvas
-    canvas.events.mouse_press(mouse_event)
-    event = Event(type='mouse_press', pos=(5051.0, 4569.0), button=2)
-    viewer.window.qt_viewer.canvas.events.mouse_press(event)
+    pos = (10, 5051.0, 4569.0)
+    viewer.cursor.position = pos
+    # create mock event objects
+    event = Mock()
+    event.button = 2  # Right mouse button
+    event.position = pos
 
-    assert track_navigation_widget.labels.selected_label == 20422
+    track_navigation_widget.select_label(viewer, event)
+
+    # Check if the label was selected correctly
+    expected_label = 20422  # Replace with the expected label for this position
+    assert track_navigation_widget.labels.selected_label == expected_label
 
 
-def test_go_to_track_beginning(qtbot,viewer,db_session):
+def test_go_to_track_beginning(qtbot, viewer, db_session):
     """
     Test moving to the beginning of the track.
     """
 
-    track_navigation_widget = TrackNavigationWidget(viewer,db_session)
+    track_navigation_widget = TrackNavigationWidget(viewer, db_session)
 
     qtbot.addWidget(track_navigation_widget)
 
@@ -111,17 +109,18 @@ def test_go_to_track_beginning(qtbot,viewer,db_session):
     track_navigation_widget.labels.selected_label = 20422
 
     # Simulate clicking the "start track" button
-    qtbot.mouseClick(track_navigation_widget.start_track_btn, Qt.LeftButton)
+    track_navigation_widget.start_track_function()
+    # qtbot.mouseClick(track_navigation_widget.start_track_btn, Qt.LeftButton)
 
     assert viewer.dims.current_step[0] == 0
 
 
-def test_go_to_track_end(qtbot, viewer,db_session):
+def test_go_to_track_end(qtbot, viewer, db_session):
     """
     Test moving to the end of the track.
     """
 
-    track_navigation_widget = TrackNavigationWidget(viewer,db_session)
+    track_navigation_widget = TrackNavigationWidget(viewer, db_session)
 
     qtbot.addWidget(track_navigation_widget)
 
@@ -132,16 +131,17 @@ def test_go_to_track_end(qtbot, viewer,db_session):
     track_navigation_widget.labels.selected_label = 20422
 
     # Simulate clicking the "start track" button
-    qtbot.mouseClick(track_navigation_widget.end_track_btn, Qt.LeftButton)
+    track_navigation_widget.end_track_function()
 
     assert viewer.dims.current_step[0] == 42
 
-def test_center_on_cell(qtbot, viewer,db_session):
+
+def test_center_on_cell(qtbot, viewer, db_session):
     """
     Test that the viewer centers on cell when requested.
     """
 
-    track_navigation_widget = TrackNavigationWidget(viewer,db_session)
+    track_navigation_widget = TrackNavigationWidget(viewer, db_session)
 
     qtbot.addWidget(track_navigation_widget)
 
@@ -152,22 +152,22 @@ def test_center_on_cell(qtbot, viewer,db_session):
     track_navigation_widget.labels.selected_label = 20422
 
     # test of default which is following the selected cell
-    np.testing.assert_allclose(viewer.camera.center,(0.0, 5051.0, 4569.0))
+    np.testing.assert_allclose(viewer.camera.center, (0.0, 5051.0, 4569.0))
 
     # test upon changing frame
     viewer.dims.set_point(0, 11)
-    np.testing.assert_allclose(viewer.camera.center,(0.0, 5049.0, 4570.0))
+    np.testing.assert_allclose(viewer.camera.center, (0.0, 5049.0, 4570.0))
 
     # test upon unclicking following
     track_navigation_widget.follow_object_checkbox.setChecked(False)
     viewer.dims.set_point(0, 10)
-    np.testing.assert_allclose(viewer.camera.center,(0.0, 5049.0, 4570.0))
+    np.testing.assert_allclose(viewer.camera.center, (0.0, 5049.0, 4570.0))
 
     # test upon clicking centering
-    qtbot.mouseClick(track_navigation_widget.center_object_btn, Qt.LeftButton)
-    np.testing.assert_allclose(viewer.camera.center,(0.0, 5051.0, 4569.0))
+    track_navigation_widget.center_object_function()
+    np.testing.assert_allclose(viewer.camera.center, (0.0, 5051.0, 4569.0))
 
     # test upon activating following
     viewer.dims.set_point(0, 11)
     track_navigation_widget.follow_object_checkbox.setChecked(True)
-    np.testing.assert_allclose(viewer.camera.center,(0.0, 5049.0, 4570.0))
+    np.testing.assert_allclose(viewer.camera.center, (0.0, 5049.0, 4570.0))
